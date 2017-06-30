23a000e232979fb964f83615dabfe956bf6a7e69
marck0793 <marck0793@hotmail.com>
2017-06-30 14:54:55 -0600

    NADA

--- DiwoApp/app/src/main/java/com/aym/android/diwoapp/MainActivity.java
+++ DiwoApp/app/src/main/java/com/aym/android/diwoapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
