2e4d7a3595acb09bcd34ff95a1c8ebd8320f8117
marck0793 <marck0793@hotmail.com>
2017-07-05 16:16:18 -0600

    Se ordena el menu, y se crean los intent

--- DiwoApp/app/src/main/java/com/aym/android/diwoapp/MainActivity.java
+++ DiwoApp/app/src/main/java/com/aym/android/diwoapp/MainActivity.java
@@ -2,14 +2,18 @@ package com.aym.android.diwoapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public Button intro;
+    private Button btnSaludos, btnEncuentros, btnOcupaciones, btnRutinas, btnEmociones;
+    private FloatingActionButton btnBattle;
 
 
     @Override
@@ -17,7 +21,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intro = (Button) findViewById(R.id.btnIntro);
+        btnSaludos = (Button)findViewById(R.id.btnSaludos);
+        btnEncuentros = (Button)findViewById(R.id.btnEncuentros);
+        btnOcupaciones = (Button)findViewById(R.id.btnOcupaciones);
+        btnRutinas = (Button)findViewById(R.id.btnRutinas);
+        btnEmociones = (Button)findViewById(R.id.btnEmociones);
+        btnBattle = (FloatingActionButton)findViewById(R.id.btnBattle);
         intro.setOnClickListener(this);
+        btnSaludos.setOnClickListener(this);
+        btnEncuentros.setOnClickListener(this);
+        btnOcupaciones.setOnClickListener(this);
+        btnRutinas.setOnClickListener(this);
+        btnEmociones.setOnClickListener(this);
+        btnBattle.setOnClickListener(this);
     }
 
     @Override
@@ -26,6 +42,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnIntro:
                 Intent intent = new Intent(MainActivity.this, IntroActivity.class);
                 startActivity(intent);
+                break;
+            default:
+                Toast.makeText(this,"Esta función no está disponible en el prototipo",Toast.LENGTH_LONG).show();
                 break;
         }
     }
--- DiwoApp/app/src/main/java/com/aym/android/diwoapp/MenuActivity.java
+++ DiwoApp/app/src/main/java/com/aym/android/diwoapp/MenuActivity.java
@@ -1,13 +1,36 @@
 package com.aym.android.diwoapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnCabecar, btnBribri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        btnCabecar = (Button)findViewById(R.id.btnCabecar);
+        btnBribri = (Button)findViewById(R.id.btnBribri);
+        btnCabecar.setOnClickListener(this);
+        btnBribri.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnCabecar:
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                Toast.makeText(this,"Esta función no está disponible en el prototipo",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
